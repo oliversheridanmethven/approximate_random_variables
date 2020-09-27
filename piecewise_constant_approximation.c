42c7882f69a1c80f8056593b69c0b024fe34093b
Oliver Sheridan-Methven <oliver.sheridan-methven@maths.ox.ac.uk>
2020-09-27 15:44:08 +0100

    Added a batch based approximation for AVX512.

--- piecewise_constant_approximation.c
+++ piecewise_constant_approximation.c
@@ -11,6 +11,7 @@
 #include "piecewise_constant_approximation_coefficients.h"
 #include <omp.h>
 
+#ifndef USE_VECTOR_BATCHES
 void piecewise_constant_approximation(unsigned int n_samples, const double *restrict input, double *restrict output)
 {
     #pragma omp simd
@@ -19,4 +20,18 @@ void piecewise_constant_approximation(unsigned int n_samples, const double *rest
         output[n] = lookup_table[(unsigned int) (LOOKUP_TABLE_SIZE * input[n])];
     }
 }
-
+#else  // Assuming we want to do this using larger AVX512 batches.
+#define VECTOR_LENGTH 8  // 8 x 64 bit doubles fit in a AVX512 vector register.
+void piecewise_constant_approximation(unsigned int n_samples, const double *restrict input, double *restrict output)
+{
+    unsigned int n_batches = n_samples / VECTOR_LENGTH;
+    for (unsigned int batch = 0; batch < n_batches; batch++, input += VECTOR_LENGTH, output += VECTOR_LENGTH)
+    {
+        #pragma omp simd simdlen(VECTOR_LENGTH)
+        for (unsigned int n = 0; n < VECTOR_LENGTH; n++)
+        {
+            output[n] = lookup_table[(unsigned int) (LOOKUP_TABLE_SIZE * input[n])];
+        }
+    }
+}
+#endif
